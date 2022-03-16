04def389fc405bb629819524707a06fcff6fe452
rubenmarinlopez80 <rubenmarinlopez@gmail.com>
2022-03-16 18:55:42 +0100

    Update DemoController.java

--- src/main/java/es/aragon/demo/controller/DemoController.java
+++ src/main/java/es/aragon/demo/controller/DemoController.java
@@ -27,6 +27,23 @@ public class DemoController {
 
     }
 	
+	private static final List<Film> DUMMY_FILMS = new ArrayList<Film>();
+	
+	static {
+		DUMMY_FILMS.add(new Film("12 years a slave", 2013, "Steve McQueen"));
+		DUMMY_FILMS.add(new Film("Argo", 2012, "Ben Affleck"));
+		DUMMY_FILMS.add(new Film("The Artist", 2011, "Michel Hazanavicius"));
+		DUMMY_FILMS.add(new Film("The King's speech", 2010, "Tom Hooper"));
+		DUMMY_FILMS.add(new Film("The Hurt Locker", 2009, "Kathryn Bigelow"));
+		DUMMY_FILMS.add(new Film("Slumdog Millionaire", 2008, "Danny Boyle"));
+		DUMMY_FILMS.add(new Film("No Country for Old Men", 2007, "Joel y Ethan Coen"));
+	}
+	
+	@GetMapping(value = "/list")
+	public List<Film> getFilms() {
+		return DUMMY_FILMS;
+	}
+	
 	@GetMapping(value = "/hola")
     public String hola(ModelMap model) {
 
