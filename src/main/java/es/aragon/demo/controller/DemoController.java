b9638f36c03cd7e257b53323799a688ec52f71b3
rubenmarinlopez80 <rubenmarinlopez@gmail.com>
2022-03-16 19:02:06 +0100

    Update DemoController.java

--- src/main/java/es/aragon/demo/controller/DemoController.java
+++ src/main/java/es/aragon/demo/controller/DemoController.java
@@ -2,6 +2,9 @@ package es.aragon.demo.controller;
 
 import java.time.LocalDateTime;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
