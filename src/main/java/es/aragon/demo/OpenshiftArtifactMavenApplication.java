b2ba1212c57ee0a28031fe55dfd50998e065d50a
rubenmarinlopez80 <rubenmarinlopez@gmail.com>
2022-03-16 20:12:46 +0100

    Update OpenshiftArtifactMavenApplication.java

--- src/main/java/es/aragon/demo/OpenshiftArtifactMavenApplication.java
+++ src/main/java/es/aragon/demo/OpenshiftArtifactMavenApplication.java
@@ -2,9 +2,23 @@ package es.aragon.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OpenshiftArtifactMavenApplication {
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftArtifactMavenApplication.class, args);
